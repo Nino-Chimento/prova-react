43fb971055103275d4d0c517f15ddd369c68ec46
Nino Chimento <saninoc84@gmail.com>
2020-03-14 16:01:16 +0100

    Revert "stampa con harrey"
    This reverts commit 1d0688c5d60fcb925b9c52f45524b802809c3e5b.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import Counter from "./counter";
 
 class Counters extends Component {
-    state = { 
-        counters : [
-            { id :1, value: 0},
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-            { id: 5, value: 0 },
-        ],
-     }
+    state = {  }
     render() { 
-        return ( <div className = "flex">
-                   {this.state.counters.map( counter => <Counter key={counter.id}/>)}
+        return ( <div className = "d-flex">
+                   <Counter />
+                   <Counter />
+                   <Counter />    
+                   <Counter />
+                   <Counter />
+
                  </div> );
     }
 }
