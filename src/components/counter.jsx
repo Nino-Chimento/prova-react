01dc9d758d42fe4bf4101d78ee58a19244447413
Nino Chimento <saninoc84@gmail.com>
2020-03-14 13:30:01 +0100

    cambio colore conteggio numero

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter  extends Component {
+    state = {
+        count : 0,
+       
+    }
+
     render() { 
+        let classesBtn = "badge m-3 badge-";
+        classesBtn += this.state.count === 0 ?"warning" : "primary";
         return (<React.Fragment>
-                 <h1>Carrello</h1>
-                 <button>Incremente</button>
+              
+                 <span className={classesBtn}>{this.formatCount()}</span>
+                 <button className ="btn btn-sm btn-dark">Incremente</button>
                </React.Fragment>
                );
     }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "zero" : count;
+    }
 }
+ 
+  
  
 export default Counter;
