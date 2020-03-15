1287a80c3aab0035ab6dbabdb7bed4c84fdbd1e9
Nino Chimento <saninoc84@gmail.com>
2020-03-15 11:32:49 +0100

    inserimento navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+
 import './App.css';
+import Counters from './components/counters';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the 'Movie Mojo' React app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <main>
+        <Counters />
+      </main>
+    </React.Fragment>
+    
   );
 }
 
